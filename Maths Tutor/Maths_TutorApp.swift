222dbb60a2dc3921211311199b0beab47329b77f
realtnt <theo@thetaphoto.co.uk>
2023-11-20 00:01:39 +0000

    Initial Commit

---
+++ Maths Tutor/Maths_TutorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Maths_TutorApp.swift
+//  Maths Tutor
+//
+//  Created by Theo Ntogiakos on 20/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Maths_TutorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
